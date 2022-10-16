c1bf7f33b265a5f71578d7eed79ec738e6cdda2b
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-10-16 14:33:58 -0300

    Retira controller inexistente

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\MemberController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\CoreController;
 
